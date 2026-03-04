5011dc5be8a63449fd30e42297590e9b35c6554a
ssbalajirao <balajiraoshanampudi@gmail.com>
2026-03-04 15:02:16 +0530

    Fix CI/CD: enable headless mode and add sandbox arguments

--- conftest.py
+++ conftest.py
@@ -6,6 +6,9 @@ from config.config import config
 @pytest.fixture(scope="function")
 def driver():
     options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     if config.HEADLESS:
         options.add_argument("--headless")
 
